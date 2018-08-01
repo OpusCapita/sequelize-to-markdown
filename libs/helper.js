2b15adedb21d930f7989c76631d75a008574bae9
Christian Drengenberg <kwierchris@users.noreply.github.com>
2018-08-01 15:54:27 +0200

    Update helper.js

--- libs/helper.js
+++ libs/helper.js
@@ -62,18 +62,16 @@ module.exports.listFiles = function(path, recursive, fileFilter, dirFilter)
 
 module.exports.mkdirp = function(path)
 {
-    let current = "";
+    let current = '';
     const subPaths = pathJs.dirname(path).split(pathJs.sep);
+    
     for(const subPath of subPaths)
     {
         current = `${current}${subPath}${pathJs.sep}`;
+        
         if(!fs.existsSync(current))
-        {
             fs.mkdirSync(current);
-        }
         else if(fs.lstatSync(current).isFile())
-        {
             throw new Error(`path '${current}' is a File.`);
-        }
     }
 };
